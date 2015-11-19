d5d0e23123c0ad5eb5cd2a6a8f616a2bf4803135
uranos <uranos.maslab@gmail.com>
2015-11-19 15:12:52 +0900

    add

--- repo/core/src/main/java/adf/agent/info/WorldInfo.java
+++ repo/core/src/main/java/adf/agent/info/WorldInfo.java
@@ -1,12 +1,16 @@
 package adf.agent.info;
 
+import com.infomatiq.jsi.Rectangle;
+import rescuecore2.misc.Pair;
 import rescuecore2.standard.entities.StandardEntity;
+import rescuecore2.standard.entities.StandardEntityURN;
 import rescuecore2.standard.entities.StandardWorldModel;
 import rescuecore2.worldmodel.ChangeSet;
 import rescuecore2.worldmodel.EntityID;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 public class WorldInfo {
@@ -38,4 +42,27 @@ public class WorldInfo {
 		return list;
 	}
 
+	public Collection<EntityID> getObjectsInRange(EntityID entity, int range) {
+		return this.convert(this.world.getObjectsInRange(entity, range));
+	}
+
+	public Collection<EntityID> getObjectsInRange(StandardEntity entity, int range) {
+		return this.convert(this.world.getObjectsInRange(entity, range));
+	}
+
+	public Collection<EntityID> getObjectsInRange(int x, int y, int range) {
+		return this.convert(this.world.getObjectsInRange(x,y,range));
+	}
+
+	public Collection<EntityID> getObjectsInRectangle(int x1, int y1, int x2, int y2) {
+		return this.convert(this.world.getObjectsInRectangle(x1, y1, x2, y2));
+	}
+
+	public Collection<EntityID> getEntitiesOfType(StandardEntityURN urn) {
+		return this.convert(this.world.getEntitiesOfType(urn));
+	}
+
+	public Collection<EntityID> getEntitiesOfType(StandardEntityURN... urns) {
+		return this.convert(this.world.getEntitiesOfType(urns));
+	}
 }
